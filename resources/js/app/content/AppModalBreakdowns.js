d8ec8b1a2788cd7eab216fd94eb1172c29d3b4ee
Rocky Eastman Jr <eastmanrjr@gmail.com>
2019-04-15 22:41:32 -0700

    Modal changes

--- resources/js/app/content/AppModalBreakdowns.js
+++ resources/js/app/content/AppModalBreakdowns.js
@@ -2,23 +2,76 @@
 // Imports
 //-----------------------------------------------------------------------------
 import React from 'react'
+import { array } from 'prop-types'
 import styled from 'styled-components'
 
 import Modal from '../components/Modal'
 //-----------------------------------------------------------------------------
 // Component
 //-----------------------------------------------------------------------------
-const AppModalBreakdowns = () => {
+const AppModalBreakdowns = ({ breakdowns }) => {
   return (
     <Modal>
-      AppModalBreakdowns
+      <BreakdownsContainer>
+        {breakdowns.map(breakdown => {
+          return (
+            <Breakdown
+              key={breakdown.id}>
+              <Column>
+                Name
+              </Column>
+              <Column>
+                Column
+              </Column>
+              <BreakdownType>
+                BreakdownType
+              </BreakdownType>
+              <Value>
+                Value
+              </Value>
+            </Breakdown>
+          )
+        })}
+      </BreakdownsContainer>
     </Modal>
   )
 }
 
 //-----------------------------------------------------------------------------
+// Props
+//-----------------------------------------------------------------------------
+AppModalBreakdowns.propTypes = {
+  breakdowns: array
+}
+AppModalBreakdowns.defaultProps = {
+  breakdowns: [
+    {id: 1},
+    {id: 2},
+    {id: 3}
+  ]
+}
+//-----------------------------------------------------------------------------
 // Styled Components
 //-----------------------------------------------------------------------------
-const Container = styled.div``
+const BreakdownsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Breakdown = styled.div`
+  padding: 2vh;
+  display: flex;
+`
+
+const Column = styled.div`
+`
+
+const BreakdownType = styled.div`
+`
+
+const Value = styled.div`
+`
 
 export default AppModalBreakdowns
